72b837145ec1a7239c77317d51717a073975f914
Mike Gindin <gindin.mike@gmail.com>
2024-08-10 16:49:42 -0400

    fix init in data_structures  / sequentional_random_access tests

--- tests/data_structures.py
+++ tests/data_structures.py
@@ -5,18 +5,24 @@ At 1_000, improvement was 2x
 At 1_000_000 improvement looks closer to 1.5x
 '''
 
-# setup
-user_ids = list(range(1_000_000))
-admin_ids_list = user_ids[:10]
-admin_ids_set = set(admin_ids_list)
+def init(size):
+    user_ids = list(range(size))
+    admin_ids_list = user_ids[:10]
+    admin_ids_set = set(admin_ids_list)
+    
+    return user_ids, admin_ids_list, admin_ids_set
 
 def test_list(size):
+    user_ids, admin_ids_list, admin_ids_set = init(size)
+    
     _ = 0
     for i in range(len(user_ids)):
         if user_ids[i] in admin_ids_list:
             _ += 1
 
 def test_set(size):
+    user_ids, admin_ids_list, admin_ids_set = init(size)
+    
     _ = 0
     for i in range(len(user_ids)):
         if user_ids[i] in admin_ids_set:
--- tests/sequental_random_access.py
+++ tests/sequental_random_access.py
@@ -1,24 +1,22 @@
 import random
 
-
-points = []
-shuffled_points = []
-
 def init(size):
     points = [{'x': 42, 'y': 0} for _ in range(size)]
 
     shuffled_points = points[:]
     random.shuffle(shuffled_points)
+    
+    return points, shuffled_points
 
 def test_sequential_access(size):
-    init(size)
+    points, shuffled_points = init(size)
     
     _ = 0
     for point in points:
         _ += point['x']
 
 def test_random_access(size):
-    init(size)
+    points, shuffled_points = init(size)
     
     _ = 0
     for point in shuffled_points:
